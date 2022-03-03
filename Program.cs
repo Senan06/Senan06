8a3eced45160f5d06f074c247d9528d7aa96d4c6
Senan06 <99797322+Senan06@users.noreply.github.com>
2022-03-03 14:17:18 +0400

    Fesil_yoxlamasi

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string ay = "cdzxfvzxcvg";
+            if (ay == "Yanvar" || ay == "Fevral" || ay == "Dekabr")Console.WriteLine("Winter");
+            else if (ay == "Mart" || ay == "Aprel" || ay == "May") Console.WriteLine("Spring");
+            else if (ay == "Iyun" || ay == "Iyul" || ay == "Avqust") Console.WriteLine("Summer");
+            else if (ay == "Sentyabr" || ay == "Oktyabr" || ay == "Noyabr") Console.WriteLine("Autumn");
+            else Console.WriteLine("Not 404 Found");
+        }
+    }
+}
